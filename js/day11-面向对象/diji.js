21a50b239113ede99890b4907b72c74011802701
mylearn-go <2733884393@qq.com>
2017-09-12 20:07:53 +0800

    jk

--- js/day11-面向对象/diji.js
+++ js/day11-面向对象/diji.js
@@ -12,7 +12,8 @@ var keyDown = false;
 
 var playPlane;  //玩家飞机
 var diJiArr = [];//存放敌机
-var ziDanArr=[];//存放子弹
+var ziDanArr = [];//存放玩家子弹
+var diziDanArr = [];//存放敌机子弹
 
 //启用定时器，开始创建多个敌机
 var diJiCreate = setInterval(createDiJi, 1000);
@@ -20,8 +21,10 @@ var diJiCreate = setInterval(createDiJi, 1000);
 var diJiMove = setInterval(moveDiJi, 200);
 //玩家飞机开始移动
 var playMove = setInterval(movePlay, 100);
-//子弹移动
-var bullet=setInterval(moveBullet,100);
+//玩家飞机子弹移动
+var bullet = setInterval(moveBullet, 100);
+//敌机子弹移动
+var dibullet=setInterval(moveDiBullet,100);
 
 createPlay();
 //移动敌机
@@ -58,19 +61,34 @@ function movePlay() {
     }
 }
 
-//子弹的移动
+//玩家飞机子弹的移动
 function moveBullet() {
-    for (var i=0;i<ziDanArr.length;i++){
-        var top=parseInt(ziDanArr[i].imgNode.style.top);
-        if(top==0){
-            mainObj.removeChild(ziDanArr[i].imgNode);
-            ziDanArr.splice(i,1);
+    for (var i = 0; i < ziDanArr.length; i++) {
+        var top = parseInt(ziDanArr[i].imgNodes.style.top);
+        if (top == 0) {
+            mainObj.removeChild(ziDanArr[i].imgNodes);
+            ziDanArr.splice(i, 1);
             i--;
-        }else {
+        } else {
             ziDanArr[i].move();
         }
     }
 }
+
+//敌机子弹的移动
+function moveDiBullet() {
+    for (var i = 0; i < diziDanArr.length; i++) {
+        var top = parseInt(diziDanArr[i].imgNodes.style.top);
+        if (top >=622) {
+            mainObj.removeChild(diziDanArr[i].imgNodes);
+            diziDanArr.splice(i, 1);
+            i--;
+        } else {
+            diziDanArr[i].move();
+        }
+    }
+}
+
 
 //创建敌机
 function createDiJi() {
@@ -81,6 +99,7 @@ function createDiJi() {
     diJiArr.push(diJi);
 }
 
+
 //创建玩家飞机
 function createPlay() {
     playPlane = new playPrototype(156, 484, "feiji/GodPlane.png", 30);
@@ -90,8 +109,8 @@ function createPlay() {
 //定义键盘事件  按下键盘的时候触发，松开键盘法时候取消触发
 document.onkeydown = function () {
     var e = window.event || arguments[0];
-    if(e.keyCode == 32){
-       playPlane.shot();
+    if (e.keyCode == 32) {
+        playPlane.shot();
     }
     if (e.keyCode == 38) {
         keyUp = true;
@@ -132,6 +151,15 @@ function diPrototype(x, y, src, speed) {
     this.imgNodes = document.createElement("img");
 
     //方法
+    this.shot = function () {
+        var top = parseInt(this.imgNodes.style.top);
+        var left = parseInt(this.imgNodes.style.left);
+        var x = left+10;
+        var y = top+20;
+        var dibullet = new dibulletPrototype(x, y, "feiji/xia.png", 10);
+        dibullet.init();
+        diziDanArr.push(dibullet);
+    };
     /**
      * 移动：相当于是控制敌机的top值  top=原来的top值+移动的速度speed
      */
@@ -166,7 +194,7 @@ function playPrototype(x, y, src, speed) {
         var width = this.imgNodes.width;
         var top = parseInt(this.imgNodes.style.top);
         var left = parseInt(this.imgNodes.style.left);
-        var x = left + width/2 - 10;
+        var x = left + width / 2 - 10;
         var y = top - 40 - 10;
         var bullet = new bulletPrototype(x, y, "feiji/bullet_03.png", 10);
         bullet.init();
@@ -210,33 +238,64 @@ function playPrototype(x, y, src, speed) {
     }
 }
 
-//创建子弹原型
+//创建玩家子弹原型
 function bulletPrototype(x, y, src, speed) {
     //属性
     this.x = x;
     this.y = y;
     this.src = src;
     this.speed = speed;
-    this.imgNode = document.createElement("img");
+    this.imgNodes = document.createElement("img");
     //方法
     /**
      * 移动：4个方向
      */
     this.move = function () {
-        var top = parseInt(this.imgNode.style.top) - this.speed;
+        var top = parseInt(this.imgNodes.style.top) - this.speed;
         if (top >= 0) {
-            this.imgNode.style.top = top + "px"
-        }else {
-            this.imgNode.style.top="0px";
+            this.imgNodes.style.top = top + "px"
+        } else {
+            this.imgNodes.style.top = "0px";
         }
     };
     /**
      *初始化(组装)
      */
     this.init = function () {
-        this.imgNode.src = this.src;
-        this.imgNode.style.left = x + "px";
-        this.imgNode.style.top = y + "px";
-        mainObj.appendChild(this.imgNode);
+        this.imgNodes.src = this.src;
+        this.imgNodes.style.left = x + "px";
+        this.imgNodes.style.top = y + "px";
+        mainObj.appendChild(this.imgNodes);
+    }
+}
+
+//创建敌军子弹原型
+function dibulletPrototype(x, y, src, speed) {
+    //属性
+    this.x = x;
+    this.y = y;
+    this.src = src;
+    this.speed = speed;
+    this.imgNodes = document.createElement("img");
+    //方法
+    /**
+     * 移动：4个方向
+     */
+    this.move = function () {
+        var top = parseInt(this.imgNodes.style.top) + this.speed;
+        if (top >= 0) {
+            this.imgNodes.style.top = top + "px";
+        } else {
+            this.imgNodes.style.top = "0px";
+        }
+    };
+    /**
+     *初始化(组装)
+     */
+    this.init = function () {
+        this.imgNodes.src = this.src;
+        this.imgNodes.style.left = x + "px";
+        this.imgNodes.style.top = y + "px";
+        mainObj.appendChild(this.imgNodes);
     }
 }
