dc7b609ae7a2d6cb8f8902a35f3606891d862d8b
mylearn-go <2733884393@qq.com>
2017-09-27 18:13:49 +0800

    jk

---
+++ projiect01/js/datebase.js
@@ -0,0 +1,23 @@
+/**
+ * Created by uid on 2017/9/27.
+ */
+var studentInfo=[];  //学生数据
+/**
+ *学生初始数据
+ */
+studentInfo=[
+    {"classNo":"140","No":1,"name":"雷云凯","sex":"男","phone":"1234","QQ":"254","gradTime":"2017","edu":"本科","gradSchool":"四川大学","conNum":"1234","fiPre":"娜姐","fiTime":"2017-7-17"},
+    {"classNo":"140","No":2,"name":"袁铭","sex":"男","phone":"25134","QQ":"251","gradTime":"2017","edu":"本科","gradSchool":"农业大学","conNum":"4567","fiPre":"娜姐","fiTime":"2017-7-17"},
+    {"classNo":"140","No":3,"name":"岳勇静","sex":"女","phone":"1364","QQ":"963","gradTime":"2017","edu":"专科","gradSchool":"四川大学","conNum":"5678","fiPre":"娜姐","fiTime":"2017-7-17"},
+    {"classNo":"140","No":4,"name":"李红燕","sex":"女","phone":"22534","QQ":"4565","gradTime":"2017","edu":"专科","gradSchool":"农业大学","conNum":"2013","fiPre":"娜姐","fiTime":"2017-7-17"},
+    {"classNo":"140","No":5,"name":"武文斌","sex":"男","phone":"1452","QQ":"789","gradTime":"2017","edu":"本科","gradSchool":"四川大学","conNum":"1234","fiPre":"娜姐","fiTime":"2017-7-17"},
+    {"classNo":"140","No":6,"name":"杨胜","sex":"男","phone":"85663","QQ":"456","gradTime":"2017","edu":"本科","gradSchool":"农业大学","conNum":"1234","fiPre":"娜姐","fiTime":"2017-7-17"},
+
+    {"classNo":"139","No":1,"name":"小云","sex":"男","phone":"1234","QQ":"254","gradTime":"2017","edu":"本科","gradSchool":"四川大学","conNum":"1234","fiPre":"陈涛","fiTime":"2017-6-25"},
+    {"classNo":"139","No":2,"name":"小铭","sex":"男","phone":"25134","QQ":"251","gradTime":"2017","edu":"本科","gradSchool":"农业大学","conNum":"4567","fiPre":"陈涛","fiTime":"2017-6-25"},
+    {"classNo":"139","No":3,"name":"小静","sex":"女","phone":"1364","QQ":"963","gradTime":"2017","edu":"专科","gradSchool":"四川大学","conNum":"5678","fiPre":"陈涛","fiTime":"2017-6-25"},
+    {"classNo":"139","No":4,"name":"小红","sex":"女","phone":"22534","QQ":"4565","gradTime":"2017","edu":"专科","gradSchool":"农业大学","conNum":"2013","fiPre":"陈涛","fiTime":"2017-6-25"},
+    {"classNo":"139","No":5,"name":"小武","sex":"男","phone":"1452","QQ":"789","gradTime":"2017","edu":"本科","gradSchool":"四川大学","conNum":"1234","fiPre":"陈涛","fiTime":"2017-6-25"},
+    {"classNo":"139","No":6,"name":"小杨","sex":"男","phone":"85663","QQ":"456","gradTime":"2017","edu":"本科","gradSchool":"农业大学","conNum":"1234","fiPre":"陈涛","fiTime":"2017-6-25"}
+
+];
