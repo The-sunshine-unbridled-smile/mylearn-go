d3247846a164bf0539630367be130dd730fdd790
smile <2733884393@qq.com>
2017-10-06 17:36:43 +0800

    work2

---
+++ projiect01/js/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Mr.袁 on 2017/9/29.
+ */
+$(".jc-manage").click(function () {
+   $(".nav-two").slideToggle();
+});
+$(".perManager").click(function () {
+   $("#frame1").attr("src","perManager.html");
+});
+$(".rl-manage").click(function () {
+    $("#frame1").attr("src","./Calendar_files/Calendar.html");
+});
